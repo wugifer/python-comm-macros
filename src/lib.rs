46d254377479a268ef2564abf84c80081913758c
wg <wg@>
2022-08-16 17:01:56 +0800

    放弃 pyo3

--- src/lib.rs
+++ src/lib.rs
@@ -7,31 +7,6 @@ use proc_macro::TokenStream;
 use quote::quote;
 
 mod afn;
-mod asx;
-
-/// AsDefaultStruct
-#[proc_macro_derive(AsDefaultStruct)]
-pub fn as_default_struct(input: TokenStream) -> TokenStream {
-    asx::as_default_struct(input)
-}
-
-/// AsPythonDict, no_simple_cov
-#[proc_macro_derive(AsPythonDict)]
-pub fn as_python_dict(input: TokenStream) -> TokenStream {
-    asx::as_python_dict(input)
-}
-
-/// AsPythonObject, no_simple_cov
-#[proc_macro_derive(AsPythonObject)]
-pub fn as_python_object(input: TokenStream) -> TokenStream {
-    asx::as_python_object(input)
-}
-
-/// AsSqlTable, no_simple_cov
-#[proc_macro_derive(AsSqlTable)]
-pub fn as_sql_table(input: TokenStream) -> TokenStream {
-    asx::as_sql_table(input)
-}
 
 /// Create a local variable __func__ = "xxx" in fn xxx()
 ///
